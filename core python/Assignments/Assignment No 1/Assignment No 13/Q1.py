14d1cad5f0bb6766417193de3a66a3c5ecb3df3c
Sayali Lad <sayalilad1803@gmail.com>
2026-01-08 13:06:33 +0530

    Add files via upload

---
+++ core python/Assignments/Assignment No 1/Assignment No 13/Q1.py
@@ -0,0 +1,8 @@
+d = {"name": "sayali", "age": 21}
+
+key = input("Enter key: ")
+value = input("Enter value: ")
+
+d[key] = value
+
+print("Updated Dictionary:", d)
---
+++ core python/Assignments/Assignment No 1/Assignment No 13/Q3.py
@@ -0,0 +1,8 @@
+d = {"name": "sayali", "age": 21, "city": "Pune"}
+
+key = input("Enter key to check: ")
+
+if key in d:
+    print("Key exists in dictionary")
+else:
+    print("Key does not exist in dictionary")
